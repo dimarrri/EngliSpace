f139960982780b200babadaa391e217f1d7a6b3e
dimarrri <dimarrri@outlook.com>
2021-03-07 21:20:11 +0300

    pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
