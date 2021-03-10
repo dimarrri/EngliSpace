df854431af782367b3049d842d1756cbb03994c3
dimarrri <dimarrri@outlook.com>
2021-03-10 22:27:34 +0300

    1221312

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { getPreparationDataWord } from "./dataWord/data";
-import logo from "./image/logo.png";
-import './App.scss';
+import { useMediaQuery } from "react-responsive";
 import classNames from 'classnames';
+import './App.scss';
 
-import language from "./image/ico/ico_language.svg";
-
+import logo from "./image/logo.png";
+import icoLanguage from "./image/ico/ico_language.svg";
+import icoLearn from "./image/ico/ico_learn.svg";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -23,22 +24,24 @@ export default class App extends React.Component {
       inputValue: "",
       currentMode: "ENG",
       hiddenPicture: true,
-      loadingContent: false,
+      loadingContent: true,
       stub: require("./image/stub.png").default,
     }
   }
-
-  // componentDidMount() {
-  //   setTimeout(() => this.setState({ ...this.state, loadingContent: false }), 3000)
-  // }
+  componentDidMount() {
+    setTimeout(() => this.setState({ ...this.state, loadingContent: false }), 3000)
+  }
   /**
   * @param { Object: event | String: event.dataset.keyMode } event 
   * @returns new state installs modes translate words. rus => eng and eng => rus ...  
   */
   handlerSwitchMode(event) {
-    if (event.target.dataset.translateLanguage === this.KEY_MODE_WORKOUT) {
+    if (event.currentTarget.dataset.translateLanguage === this.KEY_MODE_WORKOUT) {
       this.setState((prevState) => ({ ...prevState, hiddenPicture: !prevState.hiddenPicture }))
-    } else this.setState((prevState) => ({ ...prevState, currentMode: event.target.dataset.translateLanguage }))
+    } else this.setState((prevState) => ({
+      ...prevState,
+      currentMode: prevState.currentMode === this.KEY_MODE_EN ? this.KEY_MODE_RU : this.KEY_MODE_EN
+    }))
   }
   /**
   * @param { Object: event | String: event.input.value } event 
@@ -47,7 +50,6 @@ export default class App extends React.Component {
   handlerValueChangeControl(event) {
     return this.setState({ ...this.state, inputValue: event.target.value.toLowerCase().replace(/\s/g, "") })
   }
-
   /**
   * @param { Object: event.method } event process the response depending on which mode is set.
   * @returns call function to verification response user
@@ -82,48 +84,118 @@ export default class App extends React.Component {
       default: { return false }
     }
   }
+  getStringMatchPercentage(value, answer) {
+    // debugger
+    const arrLetterValue = value.split('')
+    const arrLetterAnswer = answer.split('')
+    const levelRelations = 100 / arrLetterValue.length
+    let result = []
+    for(let i = 0; i < arrLetterValue.length; ++i) {
+      const _ = arrLetterValue[i] === arrLetterAnswer[i] ? (result.push(levelRelations)) : null 
+    }
+    return result.reduce((a, b) => a + b)
+  }
+
+  checkAnswerToCurrentQuestion() {
+    const { data, inputValue, currentElement, currentMode } = this.state
+    switch (currentMode) {
+      case (this.KEY_MODE_EN): {
+        this.getStringMatchPercentage(inputValue, data[currentElement].wordEnglish)
+      }
+      case (this.KEY_MODE_RU): {
+
+      }
+    }
+  }
+
+
   render() {
-    const { data, currentElement, inputValue, currentMode, hiddenPicture, loadingContent, stub } = this.state
+
+    console.log(this.getStringMatchPercentage("abide", "adide"))
     return (
-      <div className="main-container">
-        <article className="wrapper-content-part">
-
-
-          <menu className={classNames({ ["menu-install-mode-trainer"]: true })}>
-            {[this.KEY_MODE_RU, this.KEY_MODE_EN, this.KEY_MODE_WORKOUT].map((value) => (
-              <button key={value} data-translate-language={value} onClick={this.handlerSwitchMode}><img src={language} width="30px" /></button>
-            ))}
-            <div className="stud-last-element-mode-menu"></div>
-          </menu>
-
-          <div className="container-box-word">
-
-            {loadingContent ?
-              <div className={classNames({ ['block-loaded-content']: true })}>
-                <h1>EngliSpace</h1>
-                <div className="circle-logo"><img src={logo} alt="logo" /></div>
-                <p>word world</p>
-              </div>
-              :
-              <>
-                <div className="wrapper-question-title">
-                  <h1>{currentMode == this.KEY_MODE_EN ? data[currentElement].wordEnglish : data[currentElement].wordTranslation[0]}</h1>
-                  <span>{currentMode == this.KEY_MODE_EN ? data[currentElement].wordWrongShape : null}</span>
-                </div>
-                <form className="container-input-answer" onSubmit={this.handlerCheckAnswerToQuestion}>
-                  <input placeholder="enter word" onChange={this.handlerValueChangeControl} value={inputValue} />
-                </form>
-                <div className="wrapper-for-image">
-                  {hiddenPicture ? <img src={this.state.data[currentElement].wordImage} alt="image-word" /> : <img src={stub} alt="stub" />}
-                </div>
-              </>
-            }
-
-          </div>
-
-        </article>
-      </div>
+      <HtmlMarkupApp
+        KEY_MODE_EN={this.KEY_MODE_EN}
+        KEY_MODE_RU={this.KEY_MODE_RU}
+        KEY_MODE_WORKOUT={this.KEY_MODE_WORKOUT}
+        data={this.state.data}
+        currentElement={this.state.currentElement}
+        inputValue={this.state.inputValue}
+        currentMode={this.state.currentMode}
+        hiddenPicture={this.state.hiddenPicture}
+        loadingContent={this.state.loadingContent}
+        stub={this.state.stub}
+        handlerSwitchMode={this.handlerSwitchMode}
+        handlerCheckAnswerToQuestion={this.handlerCheckAnswerToQuestion}
+        handlerValueChangeControl={this.handlerValueChangeControl}
+      />
     )
   }
 }
 
+function HtmlMarkupApp(props) {
+  const {
+    KEY_MODE_EN,
+    KEY_MODE_WORKOUT,
+    data,
+    currentElement,
+    inputValue,
+    currentMode,
+    hiddenPicture,
+    loadingContent,
+    handlerSwitchMode,
+    handlerCheckAnswerToQuestion,
+    handlerValueChangeControl,
+    stub } = props;
+
+  const isHandheldDevice = useMediaQuery({ maxWidth: "730px" })
+  const isDesktopDevice = useMediaQuery({ minWidth: "730px", maxWidth: "1900px" })
+
+  return (
+    <div className="main-container">
+      <article className={classNames({
+        ["wrapper-content-part"]: true,
+        ["wrapper-content-part-handheld"]: isHandheldDevice,
+        ["wrapper-content-part-desktop"]: isDesktopDevice,
+      })}>
+
+        <menu className={classNames({ ["menu-install-mode-trainer"]: true })}>
+          <button data-translate-language={currentMode} onClick={handlerSwitchMode}>
+            <img src={icoLanguage} width="27px" />
+          </button>
+          <button data-translate-language={KEY_MODE_WORKOUT} onClick={handlerSwitchMode}>
+            <img src={icoLearn} width="40px" />
+          </button>
+          <div className="stud-last-element-mode-menu"></div>
+        </menu>
+
+        <div className={classNames({
+          ["container-box-word"]: true,
+          ["container-box-word-handheld"]: isHandheldDevice,
+          ["container-box-word-desktop"]: isDesktopDevice,
+        })} >
+          {loadingContent ?
+            <div className={classNames({ ['block-loaded-content']: true })}>
+              <h1>EngliSpace</h1>
+              <div className="circle-logo"><img src={logo} alt="logo" /></div>
+              <p>word world</p>
+            </div>
+            :
+            <>
+              <div className="wrapper-question-title">
+                <h1>{currentMode == KEY_MODE_EN ? data[currentElement].wordEnglish : data[currentElement].wordTranslation[0]}</h1>
+                <span>{currentMode == KEY_MODE_EN ? data[currentElement].wordWrongShape : null}</span>
+              </div>
+              <form className="container-input-answer" onSubmit={handlerCheckAnswerToQuestion}>
+                <input placeholder="enter word" onChange={handlerValueChangeControl} value={inputValue} />
+              </form>
+              <div className="wrapper-for-image">
+                {hiddenPicture && data[currentElement].wordImage ? <img src={data[currentElement].wordImage} alt="image-word" /> : <img src={stub} alt="stub" />}
+              </div>
+            </>
+          }
+        </div>
+
+      </article>
+    </div>
+  )
+}
